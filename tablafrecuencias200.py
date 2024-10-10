3f42528d9a2000793cbac5e08e768b4a8392b0ef
jhonatan <jhonatanp273@gmail.com>
2024-10-10 18:47:35 -0500

    modificaciones de totales

--- tablafrecuencias200.py
+++ tablafrecuencias200.py
@@ -36,6 +36,19 @@ def calcular_frecuencias(numeros):
     for num in sorted(frec_abs.keys()):
         print(f"{num:4} | {frec_abs[num]:10} | {frec_rel[num]:10.2f} | {porcentaje[num]:10.2f} | {frec_abs_acum[num]:16} | {frec_rel_acum[num]:16.2f} | {porcentaje_acum[num]:15.2f}")
 
+    # totales
+    print("-" * 82)
+    total_frec_abs = sum(frec_abs.values())
+    total_frec_rel = sum(frec_rel.values())
+    total_porcentaje = sum(porcentaje.values())
+
+    # El último valor de la frecuencia acumulada y porcentaje acumulado es el total
+    ultimo_frec_abs_acum = max(frec_abs_acum.values())
+    ultimo_frec_rel_acum = max(frec_rel_acum.values())
+    ultimo_porcentaje_acum = max(porcentaje_acum.values())
+
+    print(f"{'Totales':4} | {total_frec_abs:10} | {total_frec_rel:10.2f} | {total_porcentaje:10.2f} | {ultimo_frec_abs_acum:16} | {ultimo_frec_rel_acum:16.2f} | {ultimo_porcentaje_acum:15.2f}")
+
 # Generar 200 números al azar entre 0 y 10
 numeros_aleatorios = [random.randint(0, 10) for _ in range(200)]
 
@@ -45,4 +58,3 @@ print(numeros_aleatorios)
 
 # Llamar a la función para calcular y mostrar las frecuencias
 calcular_frecuencias(numeros_aleatorios)
-
